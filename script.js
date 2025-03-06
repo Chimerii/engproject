5ea4b91e0dd78192de605042be0e7c50f4632401
Chimerii <lucidfr@gmail.com>
2025-03-06 13:10:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ page2a.setAttribute("src", "https://i.imgur.com/3EmOlNt.png");
 const page2b = document.getElementById("bfollow2");
 page2b.innerHTML = ("What hard work it was! I toiled like I never had before.");
 const panel2 = document.querySelector("bfollow3");
-panel2.setAttribute("onclick", "publish7()");
+panel2.setAttribute("onclick", "bpublish2()");
 }
 
 function bpublish2() {
