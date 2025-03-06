e762f591f29db6cb8403f949b02c574d695e816b
Chimerii <lucidfr@gmail.com>
2025-03-06 11:18:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ panel2.setAttribute("onclick", "publish3()");
 
 function publish3() {
 const page2a = document.getElementById("follow");
-page2a.setAttribute("src", "https://i.imgur.com/ErTRDtK.png");
+page2a.setAttribute("src", "https://i.imgur.com/un910pu.png");
 const page2b = document.getElementById("follow2");
 page2b.innerHTML = ("One of my wings was ripped off and thrown across the sea as punishment. They say angels with only one wing are evil, and we agreed I was such.");
 const panel2 = document.querySelector("button");
@@ -36,7 +36,7 @@ panel2.setAttribute("onclick", "publish4()");
 
 function publish4() {
 const page2a = document.getElementById("follow");
-page2a.setAttribute("src", "https://i.imgur.com/ErTRDtK.png");
+page2a.setAttribute("src", "https://i.imgur.com/nGSQmot.png");
 const page2b = document.getElementById("follow2");
 page2b.innerHTML = ("No evil can persist in Heaven, so I was tossed across the sea, too. Myself and my wing landed on opposite sides of the world.");
 const panel2 = document.querySelector("button");
@@ -45,7 +45,7 @@ panel2.setAttribute("onclick", "publish5()");
 
 function publish5() {
 const page2a = document.getElementById("follow");
-page2a.setAttribute("src", "https://i.imgur.com/ErTRDtK.png");
+page2a.setAttribute("src", "https://i.imgur.com/Yc2shoA.png");
 const page2b = document.getElementById("follow2");
 page2b.innerHTML = ("I scorned myself for even entertaining the thoughts I had. What had I done?");
 const panel2 = document.querySelector("button");
@@ -54,7 +54,7 @@ panel2.setAttribute("onclick", "publish6()");
 
 function publish6() {
 const page2a = document.getElementById("follow");
-page2a.setAttribute("src", "https://i.imgur.com/ErTRDtK.png");
+page2a.setAttribute("src", "https://i.imgur.com/GPjgyDx.png");
 const page2b = document.getElementById("follow2");
 page2b.innerHTML = ("After days of misery, I cast off my evil thoughts. I was atoned, but I lacked proof.");
 const panel2 = document.querySelector("button");
@@ -63,7 +63,7 @@ panel2.setAttribute("onclick", "publish7()");
 
 function publish7() {
 const page2a = document.getElementById("follow");
-page2a.setAttribute("src", "https://i.imgur.com/ErTRDtK.png");
+page2a.setAttribute("src", "https://i.imgur.com/xldhJCF.png");
 const page2b = document.getElementById("follow2");
 page2b.innerHTML = ("”If I find my wing again,” I thought, “they will see me as good again. I must find it.”");
 }
