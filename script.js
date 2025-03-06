4fb82996a447d94629a28b523e28a918ec929252
Chimerii <lucidfr@gmail.com>
2025-03-06 10:06:19 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,51 @@
+function publish() {
+const page1a = document.getElementById("follow");
+page1a.setAttribute("src", "https://i.imgur.com/qkZoqiq.png");
+const page1b = document.getElementById("follow2");
+page1b.innerHTML = ("Within me I found a joy that the others lacked.");
+const panel1 = document.querySelector("button");
+panel1.setAttribute("onclick", "publish1()");
+}
+
+function publish1() {
+const page2a = document.getElementById("follow");
+page2a.setAttribute("src", "https://i.imgur.com/Mkz9XtF.png");
+const page2b = document.getElementById("follow2");
+page2b.innerHTML = ("I ate foods the others did not eat; I sang songs they didn’t think to utter. In my foolishness I tried to tell my newfound joys to my peers…");
+const panel2 = document.querySelector("button");
+panel2.setAttribute("onclick", "publish2()");
+}
+
+function publish2() {
+const page2a = document.getElementById("follow");
+page2a.setAttribute("src", "https://i.imgur.com/ErTRDtK.png");
+const page2b = document.getElementById("follow2");
+page2b.innerHTML = ("…But they pointed out the truth. I was a blasphemer.");
+const panel2 = document.querySelector("button");
+panel2.setAttribute("onclick", "publish3()");
+}
+
+function publish3() {
+const page2a = document.getElementById("follow");
+page2a.setAttribute("src", "https://i.imgur.com/ErTRDtK.png");
+const page2b = document.getElementById("follow2");
+page2b.innerHTML = ("One of my wings was ripped off and thrown across the sea as punishment. They say angels with only one wing are evil, and we agreed I was such.");
+const panel2 = document.querySelector("button");
+panel2.setAttribute("onclick", "publish4()");
+}
+
+function publish4() {
+const page2a = document.getElementById("follow");
+page2a.setAttribute("src", "https://i.imgur.com/ErTRDtK.png");
+const page2b = document.getElementById("follow2");
+page2b.innerHTML = ("I scorned myself for even entertaining the thoughts I had. What had I done?");
+const panel2 = document.querySelector("button");
+panel2.setAttribute("onclick", "publish5()");
+}
+
+function publish5() {
+const page2a = document.getElementById("follow");
+page2a.setAttribute("src", "https://i.imgur.com/ErTRDtK.png");
+const page2b = document.getElementById("follow2");
+page2b.innerHTML = ("If I find my wing again, I thought, they will see me as good again. I must find it.");
+}
